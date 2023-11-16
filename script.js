c421309d9e3b402eb863b6b1b56e5f740f062fa0
Ghulam_muhiuddin <muhiuddinghulam825@gmail.com>
2023-11-16 19:31:51 +0500

    added fully permit

---
+++ script.js
@@ -0,0 +1,9 @@
+let barcode = document.getElementById('barcode');
+
+setInterval(() => {
+    barcode.style.border = '5px solid transparent';
+}, 700)
+
+setInterval(() => {
+    barcode.style.border = '5px solid rgb(203, 151, 77)';
+}, 1400)
